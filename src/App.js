f04d3b71728a758c814a3b9863b0685399050114
Ayush Thakur <ayushthakur189202@gmail.com>
2025-04-09 02:13:09 +0000

    Refactor App component and styles: streamline layout, enhance social links, and add resume link

--- src/App.js
+++ src/App.js
@@ -1,50 +1,31 @@
 import React from 'react';
 import './App.css';
-import { FaLinkedin, FaGithub, FaPenNib } from 'react-icons/fa';
-import { linkedIn, resumeLink, github, blog } from './myContent'; // Import your content here
+import { FaLinkedin, FaGithub, FaPenNib, FaFileAlt } from 'react-icons/fa';
+import { linkedIn, resumeLink, github, blog } from './myContent';
 
 function App() {
   return (
     <div className="container">
       <div className="intro-card">
         <h1>Hi, I'm <span className="highlight">Ayush Thakur</span></h1>
-      </div>
 
-      <div className="social-links">
-        <a
-          href={linkedIn}
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaLinkedin /> LinkedIn
-        </a>
-        <a
-          href={github}
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaGithub /> GitHub
-        </a>
-        <a
-          href={blog}
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaPenNib /> Blog
-        </a>
+        <div className="social-links">
+          <a href={linkedIn} target="_blank" rel="noreferrer">
+            <FaLinkedin /> LinkedIn
+          </a>
+          <a href={github} target="_blank" rel="noreferrer">
+            <FaGithub /> GitHub
+          </a>
+          <a href={blog} target="_blank" rel="noreferrer">
+            <FaPenNib /> Blog
+          </a>
+          <a href={resumeLink} target="_blank" rel="noreferrer">
+            <FaFileAlt /> View Resume
+          </a>
+        </div>
       </div>
-
-      <a
-        href={resumeLink}
-        target="_blank"
-        rel="noreferrer"
-      >
-        View Resume
-      </a>
     </div>
   );
 }
-
-
 
 export default App;
